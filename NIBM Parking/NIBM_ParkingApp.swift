2bc63867c5406375d6f03dba518e486e144d629a
COBScComp201p-007 <sachinlagamuwa@gmail.com>
2021-10-20 10:14:47 +0530

    Initial Commit

---
+++ NIBM Parking/NIBM_ParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIBM_ParkingApp.swift
+//  NIBM Parking
+//
+//  Created by notX on 2021-10-20.
+//
+
+import SwiftUI
+
+@main
+struct NIBM_ParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
