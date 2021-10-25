1e9c4f9a082aabc7b8cf8038b52e516f510299ff
COBScComp201p-007 <sachinlagamuwa@gmail.com>
2021-10-25 20:16:33 +0530

    Initial Commit

---
+++ SMLagamuwa-CoBScComp201p-007/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SMLagamuwa-CoBScComp201p-007
+//
+//  Created by notX on 2021-10-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SMLagamuwa-CoBScComp201p-007/SMLagamuwa_CoBScComp201p_007App.swift
@@ -0,0 +1,17 @@
+//
+//  SMLagamuwa_CoBScComp201p_007App.swift
+//  SMLagamuwa-CoBScComp201p-007
+//
+//  Created by notX on 2021-10-25.
+//
+
+import SwiftUI
+
+@main
+struct SMLagamuwa_CoBScComp201p_007App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
